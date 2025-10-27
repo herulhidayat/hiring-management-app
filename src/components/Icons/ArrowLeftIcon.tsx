f911a4caa860111ddc67fb36c06331f012f2db14
Khairul Hidayat <khairul.hidayat@jkt1.ebdesk.com>
2025-10-27 16:12:43 +0700

    [UPDATE] camera capture with 3-2-1 gestures

---
+++ src/components/Icons/ArrowLeftIcon.tsx
@@ -0,0 +1,9 @@
+export default function ArrowLeftIcon({ width = "20", height = "20" }) {
+    return(
+        <svg width={width} height={height} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M15.8334 10H4.16675" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M10.0001 15.8337L4.16675 10.0003L10.0001 4.16699" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+
+    )
+}
